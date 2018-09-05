78d6d61ae6fde43902d680766ae209c70fe19c18
NeerajUnnikrishnan <neeraju14@gmail.com>
2018-09-05 16:56:31 +0530

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      }
+    ]
+  },
+
+  {
+        test: /\.(png|jpg|gif|svg|jpeg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        },
+}
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'head'
+    })
+  ],
+  mode: 'development'
+}
